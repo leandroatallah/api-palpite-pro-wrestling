cfe662ca54e99d47e37ae26cd844175eed72ec1a
Leandro Atallah <leandroatallah@gmail.com>
2022-08-13 16:23:41 -0300

    Add cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import model
 from config import engine
 import router
@@ -6,6 +7,20 @@ import router
 model.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    'https://api-palpite-pro-wrestling.herokuapp.com/'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.router.get('/')
