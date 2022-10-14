ac8c538c85013fbbda6d9251bcb0924405c39841
Leandro Atallah <leandroatallah@gmail.com>
2022-10-14 03:34:35 -0300

    Add cors url local

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:3000",
+    "http://127.0.0.1",
+    "http://127.0.0.1:3000",
     "http://dashboard-palpite-pro-wrestling.vercel.app",
     "https://dashboard-palpite-pro-wrestling.vercel.app"
 ]
